4b98987218e6bbee9702cb4c1aa14138b24b10bd
Marco Castellacci <marco.castellacci87@gmail.com>
2022-05-29 20:39:58 -0300

    Table finished

--- JS/stats.js
+++ JS/stats.js
@@ -5,10 +5,14 @@ async function getData() {
         .then(response => response.json())
         .then(JSON => amazingEvents = JSON)
 
-    const amazing = amazingEvents.events
-    let upComingtable = document.getElementById(`upStats`);
+    const amazing = amazingEvents.events;
+    let fechaActual = amazingEvents.currentDate;
+    let upComingTable = document.getElementById(`upStats`);
     let pastTable = document.getElementById(`pastStats`);
     let table = document.getElementById("stats")
+    let eventosPasados = amazing.filter(pastEvents => pastEvents.date < fechaActual)
+    let eventosFuturos = amazing.filter(futureEvents => futureEvents.date > fechaActual)
+
 
     function arrayStats() {
         let arrayAttendance = amazing.map(evento => {
@@ -52,30 +56,74 @@ async function getData() {
     }
     tableMaxMin(arrayAttendanceMax, arrayAttendanceMin, capacity)
 
+    let categoryFuture = []
+    categoryFuture = filterCategories(eventosFuturos)
+    let categoryPast = []
+    categoryPast = filterCategories(eventosPasados)
+        // Filtramos las categorias de los eventos
 
-    let categoryTable = [];
-    let arrayCategories = new Set(arrayAttendance.map(evento => evento.category));
-    let category = [...arrayCategories]
-    category.map(category => {
-        categoryTable.push({
-            category: category,
-            evento: arrayAttendance.filter(evento => evento.category === category)
-        })
-    })
-    console.log(categoryTable);
-
-    function categoryPercentag(categoryTable) {
-        let tableHtml = ""
-        tableHtml += `<tr>
-            <td> ${categoryTable}</td> 
-            <td> ${arrayAttendance[8].name} (${arrayAttendance[8].percentage}%)</td> 
-            <td> ${capacity[0].name} - ${capacity[0].capacity}</td>   
-            </tr> `
-
-        upComingtable.innerHTML = tableHtml
-            // pastTable.innerHTML = tableHtml
+    function filterCategories(array) {
+        let categorysFiltered = array.map(evento => evento.category)
+        return [...new Set(categorysFiltered)]
     }
-    categoryPercentag(categoryTable)
+    // Calculamos el total de ingresos
+    let upComingStats = calculateStats(categoryFuture, eventosFuturos)
+    let pastStats = calculateStats(categoryPast, eventosPasados)
+
+
+    function calculateStats(arraycategories, arrayeventos) {
+        let categoryStats = []
+        let revenueCategory = 0;
+        let attendanceCategory = 0;
+        let eventsPerCategory = 0;
+
+        for (categoria of arraycategories) {
+            revenueCategory = 0;
+            attendanceCategory = 0;
+            eventsPerCategory = 0;
+            for (elemento of arrayeventos) {
+                if (elemento.category == categoria) {
+                    if (elemento.date > fechaActual) {
+                        revenueCategory += Number(elemento.price) * Number(elemento.estimate);
+                        attendanceCategory += Number(elemento.estimate) / Number(elemento.capacity) * 100;
+                        eventsPerCategory++
+                    } else {
+                        revenueCategory += Number(elemento.price) * Number(elemento.assistance);
+                        attendanceCategory += Number(elemento.assistance) / Number(elemento.capacity) * 100;
+                        eventsPerCategory++
+                    }
+
+                }
+            }
+            categoryStats.push({
+                category: categoria,
+                revenue: revenueCategory / eventsPerCategory,
+                attendance: attendanceCategory / eventsPerCategory
+            })
+            console.log(categoryStats);
+        }
+        return categoryStats;
+    }
+
+    function paintCards(array, contenedorTarjetas) {
+        contenedorTarjetas.innerHTML = "";
+
+        array.forEach(evento => {
+            contenedorTarjetas.innerHTML += `<tr>
+            <td> ${evento.category}</td> 
+            <td class="percent"> ${evento.revenue.toFixed(2)}</td> 
+            <td class="percent"> ${evento.attendance.toFixed(2)} %</td>   
+            </tr> `
+        })
+
+    }
+
+    if (pastTable != null) {
+        paintCards(pastStats, pastTable)
+    }
+    if (upComingTable != null) {
+        paintCards(upComingStats, upComingTable)
+    }
 
 }
 getData()
